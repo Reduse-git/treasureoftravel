0ea4ec057f5c8efef83da45431d04bf0465e4812
Reduse-git <den.reduse@gmail.com>
2019-12-14 11:44:27 +0200

    Initial import

---
+++ app/src/main/java/com/reduse/treasureoftravel/TravelActivityList.java
@@ -0,0 +1,14 @@
+package com.reduse.treasureoftravel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TravelActivityList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+    }
+}
